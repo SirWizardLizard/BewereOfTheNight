3dc2b097e79b145fe1a0e442d2eb8b1ae8a74a6a
josh-patino <83191880+josh-patino@users.noreply.github.com>
2022-02-09 16:02:37 -0600

    health .2 update

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,77 @@
+﻿//This script is based on https://www.youtube.com/watch?v=3uyolYVsiWc
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int health;
+    public int maxHealth;
+
+    public List<Image> hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    public bool gameOver = false;
+
+    public GameObject gameOverText;
+
+    void Update()
+    {
+        //If health is somehow more than max health, set health to max health
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+
+
+        for (int i = 0; i < hearts.Count; i++)
+        {
+            //Display full or empty heart sprite based on current health
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            //Show the number of hearts equal to current max health
+            if (i < maxHealth)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+
+        if (health <= 0)
+        {
+            gameOver = true;
+            gameOverText.SetActive(true);
+
+            //Press R to restart if game is over
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+            }
+        }
+
+    }
+
+    public void TakeDamage()
+    {
+        health--;
+    }
+
+    public void AddMaxHealth()
+    {
+        maxHealth++;
+    }
+
+
+}
