153272c4789627cf0d2299b9446f0a176f32383c
King-Julian11 <blazerman741@gmail.com>
2022-02-23 22:29:07 -0600

    Power Up Scripts

---
+++ Assets/Scripts/AttackPowerUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackPowerUp : MonoBehaviour
+{
+    public PlayerAttackDamage damageScript;
+    private bool triggered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        damageScript = GameObject.FindObjectOfType<PlayerAttackDamage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider other)
+    {
+        if (other.CompareTag("Attack") && !triggered)
+        {
+
+        }
+    }
+}
