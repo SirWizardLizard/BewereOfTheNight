ca7a8b15a1db8856231013d6565057c359e110d8
Zech Burrus <zechburrus@gmail.com>
2022-02-27 22:26:41 -0600

    kill counter working

---
+++ Assets/Scripts/DisplayKillCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayKillCounter : MonoBehaviour
+{
+    public Text textbox;
+    public int killCounter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textbox = GetComponent<Text>();
+        textbox.text = "Kills: 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textbox.text = "Kills: " + killCounter;
+    }
+}
